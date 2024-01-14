3650834c6ea2859f6813ee4321da959f353ba68d
Lucas Kaczmarski <lucaskacz1@gmail.com>
2024-01-14 20:15:42 +0100

    Add file transcription on upload

--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 #Form validation and rendering library
 from wtforms import FileField, SubmitField
+from wtforms.validators import InputRequired
 from werkzeug.utils import secure_filename
 import os
 
@@ -28,7 +29,7 @@ mel = whisper.log_mel_spectrogram(audio).to(model.device)
 nltk.download('punkt')
 
 class UploadFileForm(FlaskForm):
-    file = FileField("Field")
+    file = FileField("Field", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
 
@@ -40,11 +41,35 @@ def hello_world():
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     form = UploadFileForm()
+    file_info = None
+    
     if form.validate_on_submit():
         file = form.file.data #We retrieve the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename))) #Then we save the file
-        return "File has been uploaded."
-    return render_template('index.html', form=form)
+        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+        file.save(file_path) #Then we save the file
+
+        audio = whisper.load_audio(file_path)
+        audio = whisper.pad_or_trim(audio)
+        duration = len(audio) / 1000
+
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        _, probs = model.detect_language(mel)
+        print(f"Detected language: {max(probs, key=probs.get)}")
+
+        options = whisper.DecodingOptions(fp16=False)
+        result = whisper.decode(model, mel, options)
+
+        print("Audio duration:", duration)
+
+        file_info = {
+            'file_path': file_path,
+            'result_text': result.text,
+        }
+
+    #     return f"File has been uploaded. Path: {file_path}, {result.text}"
+    # return render_template('index.html', form=form)
+    return render_template('index.html', form=form, file_info=file_info)
 
 @app.route("/transcript")
 def transcript():

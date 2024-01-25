f580f97f0d73249bd746d2a315408f959b5fee5b
SnyDrive25 <sunilgouvi@gmail.com>
2024-01-25 22:04:00 +0100

    removed unnecessary code and added flask_cors and flask_wtf to requirements

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, Response
 
 # CORS
+from flask_wtf import FlaskForm
 from flask_cors import CORS
 
 # Forms
@@ -44,94 +45,7 @@ class UploadFileForm(FlaskForm):
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-# Upload files function
-
-
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    form = UploadFileForm()
-    file_info = {
-        'file_path': None,
-        'result_text': "No input data"
-    }
-
-    if form.validate_on_submit():
-        file = form.file.data  # We retrieve the file
-        file_path = os.path.join(os.path.abspath(os.path.dirname(
-            __file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
-        file.save(file_path)  # Then we save the file
-
-        audio = whisper.load_audio(file_path)
-        # audio = whisper.pad_or_trim(audio)
-        duration = len(audio) / 1000
-
-        # mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-        # _, probs = model.detect_language(mel)
-        # print(f"Detected language: {max(probs, key=probs.get)}")
-
-        # options = whisper.DecodingOptions(fp16=False)
-        # result = whisper.decode(model, mel, options)
-
-        # print("Audio duration:", duration)
-
-        coupures = []
-
-        for i in range(int(duration / 121) + 1):
-            coupures.append(
-                [max(i*121 - 5, 0), min((i+1) * 121, int(duration))])
-
-        # print("Coupures:", coupures)
-
-        all_text = ""
-
-        for i in range(len(coupures)):
-            # print("Coupure:", coupures[i])
-
-            cut_audio = audio[coupures[i][0] * 1000: coupures[i][1] * 1000]
-
-            # Convert audio to mono
-            cut_audio2 = whisper.pad_or_trim(cut_audio)
-
-            # print("Cut audio:", cut_audio2)
-
-            mel = whisper.log_mel_spectrogram(
-                cut_audio2).to(model.device)
-            _, probs = model.detect_language(mel)
-            # print(f"Detected language: {max(probs, key=probs.get)}")
-
-            options = whisper.DecodingOptions(fp16=False)
-            result = whisper.decode(model, mel, options)
-
-            print(f"{100*i/len(coupures):.2f}% : {result.text}")
-
-            all_text += result.text
-
-        print("All text:", all_text)
-
-        file_info = {
-            'file_path': file_path,
-            'result_text': all_text
-        }
-    # return render_template('index.html', form=form, file_info=file_info)
-    return json.dumps({'result_text': file_info['result_text']})
-
-
-@app.route("/summarize", methods=["GET", "POST"])
-def summarize():
-    form = UploadFileForm()
-    if request.method == "POST":
-        result_text = request.form.get("result_text")
-        if request.form.get("num_sentences") is None:
-            num_sentences = 2
-        else:
-            num_sentences = int(request.form.get("num_sentences"))
-        if request.form.get("language") is None:
-            language = "english"
-        else:
-            language = request.form.get("language")
+    return "<p>Welcome to ListenAI - By KACZMARSKI Lucas & GOULAMHOUSSEN Sunil</p>"
 
 
 @app.route("/upload", methods=['POST'])
